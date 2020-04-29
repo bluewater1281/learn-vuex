1197667342a6e605bf85f957d4ffdf4f904ec117
liwd <liwd@mapbar.com>
2020-04-30 03:10:20 +0800

    安装vuex和了解vuex是什么

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+      count: 0,
+    },
+    mutations: {
+      increment(state) {
+        state.count++
+      }
+    }
+})
+
+store.commit('increment');
+console.log('store.state.count',store.state.count);
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
